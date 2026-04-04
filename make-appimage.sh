2dc9622a9fef43bd482722538c7dda6422c39991
Link4Electronics <link4electronics@gmail.com>
2026-04-04 05:01:18 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q abaddon | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/
+export DESKTOP=/usr/share/applications/abaddon.desktop
+export DEPLOY_GTK=1
+export GTK_DIR=gtk-3.0
+export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE /usr/share/abaddon
 
 # Additional changes can be done in between here
 
