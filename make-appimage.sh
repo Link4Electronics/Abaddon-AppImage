1680b4e9864dcb4eb8e41788ace3d6b44cc4d4e7
Link4Electronics <link4electronics@gmail.com>
2026-04-04 05:25:18 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/
+export ICON=https://raw.githubusercontent.com/uowuo/abaddon/refs/heads/master/res/desktop/icon.svg
 export DESKTOP=/usr/share/applications/abaddon.desktop
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-3.0
